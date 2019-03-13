135be443962ae5a8fcab09a55aac5291b9b45b8d
sakshi <you@example.com>
2019-03-13 20:34:44 +0530

    Lectur 30

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,6 +1,18 @@
+//LECTURE 30
+
 console.log("app of weather app starting");
 
 setTimeout(() => {
   console.log("2 seconds break it is");
-},2000)
+},2000);
+setTimeout(() => {
+  console.log("0 seconds break it is");
+},0);
 console.log("app of weather app stopping");
+//OUTPUT
+/*
+app of weather app starting
+app of weather app stopping
+0 seconds break it is
+2 seconds break it is
+*/
