6da070146e4aaf779e96a302b3651df0dbb6d796
sakshi <you@example.com>
2019-03-15 17:15:24 +0530

    Express STart

---
+++ node-course/web-server/src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+
+app.get('', (req,res) => {
+  res.send("<h1>Hello EXpress</h1>");
+})
+app.get('/help', (req,res) => {
+  res.send([{
+    name: 'Sakshi',
+  },{
+    name: 'VArun'
+  }]);
+})
+app.get('/about', (req,res) => {
+  res.send({
+    name: 'Sakshi',
+    subject: 'EXpress'
+  });
+})
+app.get('/weather', (req,res) => {
+  res.send({
+    loaction: 'Philadelphia',
+    temperature:15
+  });
+})
+
+
+
+
+app.listen(3000,() => {
+  console.log(":SErver is up");
+})
