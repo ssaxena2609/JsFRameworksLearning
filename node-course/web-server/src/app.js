298fe04bc76f5393304f676a03fd55ad57f4e0cf
sakshi <you@example.com>
2019-03-15 20:09:05 +0530

    Handlebars Templates

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -6,6 +6,27 @@ const app = express();
 
 const publicDirectoryPath = path.join(__dirname,'../public');
 app.use(express.static(publicDirectoryPath));
+app.set('view engine','hbs');
+
+app.get('', (req,res) => {
+  res.render("index",{
+    title: "weather app",
+    name:"Sakshi"
+  });
+})
+
+app.get('/about', (req,res) => {
+  res.render("about",{
+    description: "THis app is all about displaying weather for your city",
+    temperature:"45"
+  });
+})
+
+app.get('/help',(req,res) => {
+  res.render('help', {
+    ContactInfo: 1234567
+  });
+})
 
 // app.get('', (req,res) => {
 //   res.send("<h1>Hello EXpress</h1>");
